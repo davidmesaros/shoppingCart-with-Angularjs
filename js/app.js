2b3349c8409b3579c43c38880c3949a054cfa3b1
davidmesaros <david.mesaros@hotmail.com>
2014-09-15 16:34:50 +1000

    added images to angular

--- js/app.js
+++ js/app.js
@@ -12,14 +12,26 @@ var gems = [
 	price: 2.95,
 	description: 'The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brothers keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.',
 	purchase: true, // adding button
-	soldOut: true,
+	soldOut: false,
+	images:[
+			{
+				full: 'images/raphal.png',
+				thumb:'images/raphal.png'
+			},
+			{
+				full: 'images/pasta.jpg',
+				thumb: 'images/pasta.jpg'
+			},
+			]
+
 	},
 	{
 	name: 'David',
 	price: 3.95,
 	description: 'The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brothers keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.',
-	purchase: false, // adding button
-	soldOut: true,
+	purchase: true, // adding button
+	soldOut: false,
+		
 	},
 ];
 })();
